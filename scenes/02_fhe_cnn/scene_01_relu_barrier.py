a7f922b06c590e03a20c6a23d371f55dc6e07291
Le Han <thlehan.cyberdata@gmail.com>
2026-06-09 19:32:36 +0700

    fix: remove duplicate code to resolve conflict

--- scenes/02_fhe_cnn/scene_01_relu_barrier.py
+++ scenes/02_fhe_cnn/scene_01_relu_barrier.py
@@ -1,38 +1,4 @@
 import sys
-sys.path.append('.')
-
-from manim import *
-from scenes.library.constants import *
-from scenes.library.ehe_primitives import CiphertextBlock, PlaintextBlock
-
-class ReLuBarrier(Scene):
-    def construct(self):
-        # 1. Đồng bộ Audio (Ghi chú: Thay tên file thực tế của bạn)
-        # self.add_sound("assets/audio/act2_scene01_relu_barrier.mp3")
-        
-        # 2. Tạo Node ReLU
-        relu_node = Circle(radius=1.5, color=COLOR_PLAINTEXT).set_fill(COLOR_PLAINTEXT, opacity=0.2)
-        relu_label = MathTex(r"\max(0, x)", font_size=48, color=COLOR_MATH)
-        node_group = VGroup(relu_node, relu_label).shift(RIGHT * 3)
-        
-        self.play(DrawBorderThenFill(relu_node), Write(relu_label))
-        self.wait(1)
-
-        # 3. CiphertextBlock tiến vào Node
-        ctx_block = CiphertextBlock(label_text="FHE Data").shift(LEFT * 4)
-        self.play(FadeIn(ctx_block))
-        
-        # Di chuyển khối mã hóa đến hàm ReLU
-        self.play(ctx_block.animate.next_to(node_group, LEFT, buff=0.5), run_time=2)
-        
-        # 4. Báo lỗi: Đổi màu Node sang Đỏ và thêm dấu X
-        error_cross = Cross(node_group, stroke_color=RED_C, stroke_width=8)
-        self.play(
-            relu_node.animate.set_color(RED_C).set_fill(RED_C, opacity=0.4),
-            Create(error_cross)
-        )
-        self.wait(2)
-
 sys.path.append(".")
 
 from manim import *
--- scenes/02_fhe_cnn/scene_02_polynomial_approx.py
+++ scenes/02_fhe_cnn/scene_02_polynomial_approx.py
@@ -1,41 +1,4 @@
 import sys
-sys.path.append('.')
-
-from manim import *
-from scenes.library.constants import *
-from scenes.library.ehe_primitives import CiphertextBlock, PlaintextBlock
-
-class PolynomialApproximation(Scene):
-    def construct(self):
-        # Thiết lập trục tọa độ
-        axes = Axes(
-            x_range=[-3, 3, 1],
-            y_range=[-1, 3, 1],
-            axis_config={"color": GREY_C}
-        )
-        axes_labels = axes.get_axis_labels(x_label="x", y_label="y")
-        
-        self.play(Create(axes), FadeIn(axes_labels))
-        
-        # Đồ thị ReLU gốc (Đường gãy khúc)
-        relu_graph = axes.plot(lambda x: max(0, x), color=RED_C, use_smoothing=False)
-        relu_label = MathTex(r"f(x) = \max(0, x)", color=RED_C).to_corner(UL)
-        
-        self.play(Create(relu_graph), Write(relu_label))
-        self.wait(2) # Khớp với lời thoại giải thích rào cản
-        
-        # Đồ thị Đa thức xấp xỉ (Đường cong) - Ví dụ: y = 0.1x^2 + 0.5x + 0.4
-        poly_graph = axes.plot(lambda x: 0.125 * x**2 + 0.5 * x + 0.25, color=COLOR_ENCRYPTION)
-        poly_label = MathTex(r"P(x) = ax^2 + bx + c", color=COLOR_ENCRYPTION).to_corner(UL)
-        
-        # Hiệu ứng "Bẻ cong": Biến đổi đồ thị và công thức
-        self.play(
-            ReplacementTransform(relu_graph, poly_graph),
-            ReplacementTransform(relu_label, poly_label),
-            run_time=3
-        )
-        self.wait(2)
-
 sys.path.append(".")
 
 from manim import *
